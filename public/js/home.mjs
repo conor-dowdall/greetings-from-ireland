d06522439e811781f43a7d5eea21a53d89dd8c4a
Conor Dowdall <conordowdallmail@gmail.com>
2024-04-19 19:01:55 +0100

    fix when carouself is not loaded

--- public/js/home.mjs
+++ public/js/home.mjs
@@ -1,4 +1,7 @@
 const items = document.querySelectorAll(".carousel-item");
-const randomItem = Math.floor(Math.random() * items.length);
-items.forEach((item) => item.classList.remove("active"));
-items[randomItem].classList.add("active");
+
+if (items?.length) {
+  const randomItem = Math.floor(Math.random() * items.length);
+  items.forEach((item) => item.classList.remove("active"));
+  items[randomItem].classList.add("active");
+}
