32ad4989f9265def3f15a0b89f049ac5e413fbed
Conor Dowdall <conordowdallmail@gmail.com>
2024-04-21 13:30:05 +0100

    improve greetings interface

--- greetings_from_ireland.sql
+++ greetings_from_ireland.sql
@@ -36,5 +36,14 @@ CREATE TABLE orders (
 INSERT INTO products 
 VALUES 
 (NULL, 'Alright', 'A simple Irish greeting, saying "Alright?"', 0, 'alright.mp3'),
-(NULL, 'Howaya', 'A simple Irish greeting, saying "How are you?"', 0.5, 'howaya.mp3'),
-(NULL, 'Craic', 'A simple Irish greeting, saying "What''s the craic?"', 1.0, 'craic.mp3');
+(NULL, 'Top Of The Mornin''', 'An Irish greeting, wishing you a top morning!', 0, 'totm.mp3'),
+(NULL, 'Howaya', 'A simple Irish greeting, asking how you are.', 0.5, 'howaya.mp3'),
+(NULL, 'Craic', 'An Irish greeting, asking if there''s any craic.', 0.5, 'craic.mp3'),
+(NULL, 'Well', 'A simple Irish greeting, saying "Well?"', 0.5, 'well.mp3'),
+(NULL, 'Mother and Father', 'An Irish greeting, checking on your mother and father''s health.', 1.0, 'motherFather.mp3'),
+(NULL, 'Strange or Startling', 'An Irish greeting, checking how things are with you. ', 1.0, 'strangeStartling.mp3'),
+(NULL, 'How Are You?', 'An Irish greeting, asking how you are.', 5.0, 'howAreYou.mp3'),
+(NULL, 'Cuttin''', 'An Irish greeting, asking how you are.', 5.0, 'cutting.mp3'),
+(NULL, 'Great To See You', 'An Irish greeting, asking how you have been."', 5.0, 'greatToSeeYou.mp3'),
+(NULL, 'Blessing', 'A long Irish greeting, giving you a blessing.', 10.0, 'blessing.mp3'),
+(NULL, 'Irish Phrase', 'An Irish greeting, in the Irish language, hoping your road will be a successful one!', 10.0, 'irishRoad.mp3');
