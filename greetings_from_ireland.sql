5c7140473c4d9fd529f73ace2fee325f73654e1c
Conor Dowdall <conordowdallmail@gmail.com>
2024-04-22 14:09:09 +0100

    add unique email constraint

--- greetings_from_ireland.sql
+++ greetings_from_ireland.sql
@@ -12,7 +12,8 @@ CREATE TABLE users (
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (user_id)
+    PRIMARY KEY (user_id),
+    UNIQUE (email)
 );
 
 CREATE table subscribers (
