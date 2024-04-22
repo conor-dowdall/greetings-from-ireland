b5b66b72382bc6c8b367212adb6c0c8220e0b816
Conor Dowdall <conordowdallmail@gmail.com>
2024-04-22 14:27:22 +0100

    add comments

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import cookieParser from "cookie-parser";
 import pagesRouter from "./routes/pages.js";
 import authRouter from "./routes/auth.js";
 
+// create a database connection using .env file info
 const db = mysql8.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -16,30 +17,39 @@ const db = mysql8.createConnection({
   database: process.env.DB_NAME,
 });
 
+// connect to the databsae
 db.connect((error) => {
   if (error) console.error(error);
   else console.log(`Connected to database:\t${process.env.DB_NAME}`);
 });
 
+// create an express app
 const app = express();
 
+// use handlebars for templating
 app.set("view engine", "hbs");
 
+// create a handlebars helper to display a number in a currency format
+// e.g. 1.5 -> 1.50; 10 -> 10.00
 handlebars.registerHelper("toCurrency", function (number) {
   return number.toFixed(2);
 });
 
+// serve the favicon
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(serveFavicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "public")));
 
+// tell express what we will be using
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
+// add some Routers to express
 app.use("/", pagesRouter);
 app.use("/auth", authRouter);
 
+// start the server listening on the port defined in .env
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server started on port:\t${process.env.SERVER_PORT}`);
 });
