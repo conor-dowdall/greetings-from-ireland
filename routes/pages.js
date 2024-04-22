540c2cbcfa36e064aee5af21544a9efa2a355ac7
Conor Dowdall <conordowdallmail@gmail.com>
2024-04-22 14:22:14 +0100

    add comments and remove unnecessary getProducts call in /profile post

--- routes/pages.js
+++ routes/pages.js
@@ -9,6 +9,8 @@ import {
 const router = express.Router();
 
 router.get("/", isLoggedIn, (req, res) => {
+  // if a user is logged in, req.user is present
+  //   pass that info to the home page, to be displayed
   if (req.user) res.render("home", { user: req.user });
   else res.render("home");
 });
@@ -16,22 +18,42 @@ router.get("/", isLoggedIn, (req, res) => {
 router.post("/subscribe", subscribeEmail);
 
 router.get("/login", isLoggedIn, (req, res) => {
-  if (req.user) res.render("profile", { user: req.user });
+  // if a user is logged in, req.user is present
+  //   pass that info to the profile page, so the
+  //   relevant user profile can be displayed
+  // otherwise, show the login page
+  if (req.user) res.redirect("/profile");
   else res.render("login");
 });
 
 router.get("/register", isLoggedIn, (req, res) => {
+  // if a user is logged in, req.user is present
+  //   pass that info to the profile page, so the
+  //   relevant user profile can be displayed
+  // otherwise, show the login page
   if (req.user) res.redirect("/profile");
   else res.render("register");
 });
 
 router.get("/profile", isLoggedIn, getProducts, (req, res) => {
+  // if a user is logged in, req.user is present
+  //   we then get the user's products/purchases info
+  //   and pass that info to the profile page, so the
+  //   relevant user profile with purchasable/purchased products
+  // otherwise, show the login page
   if (req.user)
     res.render("profile", { user: req.user, products: req.products });
   else res.redirect("/login");
 });
 
-router.post("/profile", isLoggedIn, buyProduct, getProducts, (req, res) => {
+router.post("/profile", isLoggedIn, buyProduct, (req, res) => {
+  // if a user is logged in, req.user is present
+  //   we can then buy the selected greeting
+  //   by updating the orders table in the database
+  //   then reload the profile page, which will update the
+  //   greetings list, allowing the purchased greeting to be played
+  //   instead of purchased
+  // otherwise, show the login page
   if (req.user) res.redirect("/profile");
   else res.redirect("/login");
 });
